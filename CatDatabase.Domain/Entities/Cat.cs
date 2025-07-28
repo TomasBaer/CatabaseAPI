c942bc20de56c2fbe4094d76227deb5c5c59ca5a
Tomas Baer <tpbaer@protonmail.com>
2025-07-28 20:29:46 +0100

    Added Domain project (plus other stuff)

---
+++ CatDatabase.Domain/Entities/Cat.cs
@@ -0,0 +1,10 @@
+﻿namespace CatDatabase.Domain.Entities;
+
+public class Cat
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public string Breed { get; set; } = string.Empty;
+	public string Color { get; set; } = string.Empty;
+	public int Age { get; set; }
+}
---
+++ CatDatabase.Domain/UseCases/IRegisterCat.cs
@@ -0,0 +1,6 @@
+﻿namespace CatDatabase.Domain.UseCases;
+
+public interface IRegisterCat
+{
+	Task RegisterCatAsync(string name, string breed, string color, int age);
+}
