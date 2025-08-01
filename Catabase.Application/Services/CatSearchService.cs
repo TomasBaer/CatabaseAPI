d071b921fffacdc2efb11f30ecc38449297f84e5
Tomas Baer <117166151+TomasBaer@users.noreply.github.com>
2025-08-01 17:15:43 +0100

    Added logging to method

--- Catabase.Application/Services/CatSearchService.cs
+++ Catabase.Application/Services/CatSearchService.cs
@@ -3,16 +3,22 @@ using Catabase.Application.Responses;
 using Catabase.Domain.Entities;
 using Catabase.Domain.Responses;
 using Catabase.Domain.UseCases;
+using Microsoft.Extensions.Logging;
 
 namespace Catabase.Application.Services;
 
-public class CatSearchService(ICatRepository catRepository) : ICatSearch
+public class CatSearchService(ICatRepository catRepository, ILogger<CatSearchService> logger) : ICatSearch
 {
 	private readonly ICatRepository _catRepository = catRepository;
+	private readonly ILogger<CatSearchService> _logger = logger;
 
 	public async Task<ISearchResponse<Cat>> SearchCatsAsync(string query, int page, int pageSize, CancellationToken ct = default)
 	{
+		_logger.LogInformation("Searching for cats with query: {Query}, page: {Page}, pageSize: {PageSize}", query, page, pageSize);
+
 		var searchResults = await _catRepository.SearchCatsAsync(query, page, pageSize, ct);
+
+		_logger.LogInformation("Found {TotalCount} cats matching the query.", searchResults.Item2);
 
 		return new CatSearchResponse()
 		{
